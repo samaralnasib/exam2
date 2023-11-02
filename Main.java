c5c2afaca9823a1a849fada8c6e8e0ccbf5abe1f
samaralnasib <148685912+samaralnasib@users.noreply.github.com>
2023-11-02 11:39:43 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        /*
+        System.out.println("First number:");
+        int firstnum = input.nextInt();
+        System.out.println("Second number:");
+        int secondnum = input.nextInt();
+        System.out.println("theird number:");
+        int theirdnum = input.nextInt();
+
+        int a=firstnum;
+        int b=secondnum;
+        int c=theirdnum;
+        if (a>b ){
+            System.out.println("biggest number");
+
+        } else if (a>c) {
+            System.out.println("biggest number");
+
+        } else System.out.println("another ");
+
+        }
+
+         */
+        /*
+        int cont = 0;
+        int namas = 0;
+        String names = input.next();
+        for (int i = 0; i < names.length(); i++) {
+            System.out.println("Enter name:");
+            if (namas > cont) {
+                System.out.println(namas);
+            } else System.out.println(names.length());
+
+        }
+    }
+}
+
+         */
+        /*
+        System.out.println("enter a string:");
+        String str = input.next();
+        int a=0;
+        for (int i=0; i<str.length(); i++){
+            System.out.println(str.contains("a"));
+
+        }
+        
+         */
+
+
+
+
+    }
+}
